a45a6499552d16a9d81aa86be43fa9096381fcfb
Abdul <samad3911@gmail.com>
2021-08-24 10:16:05 +0530

    Operating with the list data types

---
+++ list_operations.py
@@ -0,0 +1,35 @@
+seconds = [1.234597, 1.3578964, 2.78984984]
+
+current = 3.457898382
+
+print(seconds)
+
+seconds.append(current)
+
+print(seconds)
+
+seconds.remove(current)
+
+print(seconds)
+
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[2])
+
+print(serials[0], serials[2], serials[5])
+
+workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+
+print(workdays)
+
+workdays.append(weekend[0])
+
+print(workdays)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+print(letters[1:4])
+
+print(letters[0:3])
+
+print(letters[-3:])
